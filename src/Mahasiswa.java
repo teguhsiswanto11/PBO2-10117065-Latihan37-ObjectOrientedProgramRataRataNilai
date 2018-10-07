e7be9d9154bb414c9aeebe978b3e6b6d060ed93f
Teguh Siswanto <tsiswanto73@gmail.com>
2018-10-07 08:11:56 +0700

    Initial commit

---
+++ src/Mahasiswa.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Mahasiswa {
+    public int nilaiMhs;
+    public double totalNilaiMhs;
+    Scanner scn = new Scanner(System.in);
+
+    public double HitungTotal(int parNilaiMhs, int parN){
+        for (int i=1; i<=parN; i++){
+            System.out.print("Nilai Mahasiswa ke-"+i+" : ");
+            parNilaiMhs = scn.nextInt();
+            totalNilaiMhs += parNilaiMhs;
+        }
+    return totalNilaiMhs;}
+
+    public double HitungRataRataNilaiMhs(double total, int jmlhMhs) {
+        return total/jmlhMhs;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * @author
+ * NAMA     : Teguh Siswanto
+ * KELAS    : PBO2
+ * NIM      : 10117065
+ * Deskripsi Program : Mengisi nilai mahasiswa yang banyaknya ditentukan
+ * oleh inputan kita sendiri kemudian menampilkan rata-rata nilainya dengan
+ * konsep OOP
+ *
+ */
+public class main {
+
+    public static void main(String[] args) {
+         Mahasiswa mah = new Mahasiswa();
+         Scanner scn = new Scanner(System.in);
+
+         int n;
+         System.out.print("Masukkan Banyaknya Mahasiswa : ");
+         n = scn.nextInt();
+
+         mah.HitungTotal(mah.nilaiMhs, n);
+         mah.HitungRataRataNilaiMhs(mah.totalNilaiMhs, n);
+
+        System.out.println("\nMaka, Rata-rata Nilainya adalah : "+mah.HitungRataRataNilaiMhs(mah.totalNilaiMhs,n));
+        System.out.println("Developed by : Rizki Adam Kurniawan");
+
+    }
+}
